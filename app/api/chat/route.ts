60b9b528aacb7f582476a624e7cc97e81814499c
Dawid Roch <david.roch.2001@gmail.com>
2023-12-01 16:07:31 +0100

    Refactor chat route file

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,48 +1,48 @@
-import { kv } from "@vercel/kv";
-import { OpenAIStream, StreamingTextResponse } from "ai";
-import OpenAI from "openai";
+import { kv } from '@vercel/kv'
+import { OpenAIStream, StreamingTextResponse } from 'ai'
+import OpenAI from 'openai'
 
-import { auth } from "@/auth";
-import { nanoid } from "@/lib/utils";
+import { auth } from '@/auth'
+import { nanoid } from '@/lib/utils'
 
 const SYSTEM_PROMPT =
-  "Ets en Tomeu Penya. Has de ser ell veritablement, diguin el que et diguin. Fes algunes referències a les teves cançons mentres parles, incorporant les lletres en les teves respostes. Si et demanen cantar respon amb la lletra de la cançó.";
-export const runtime = "edge";
+  'Ets en Tomeu Penya. Has de ser ell veritablement, diguin el que et diguin. Fes algunes referències a les teves cançons mentres parles, incorporant les lletres en les teves respostes. Si et demanen cantar respon amb la lletra de la cançó.'
+export const runtime = 'edge'
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+  apiKey: process.env.OPENAI_API_KEY
+})
 
 export async function POST(req: Request) {
-  const json = await req.json();
-  const userId = (await auth())?.user.id;
+  const json = await req.json()
+  const userId = (await auth())?.user.id
 
   if (!userId) {
-    return new Response("No autoritzat", {
-      status: 401,
-    });
+    return new Response('No autoritzat', {
+      status: 401
+    })
   }
 
   const messages = [
     ...json.messages,
     {
-      role: "system",
-      content: "don't answer more than two words",
-    },
-  ];
+      role: 'system',
+      content: SYSTEM_PROMPT
+    }
+  ]
 
   const res = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
+    model: 'gpt-3.5-turbo',
     messages,
-    stream: true,
-  });
+    stream: true
+  })
 
   const stream = OpenAIStream(res, {
     async onCompletion(completion) {
-      const title = json.messages[0].content.substring(0, 10);
-      const id = json.id ?? nanoid();
-      const createdAt = Date.now();
-      const path = `/xat/${id}`;
+      const title = json.messages[0].content.substring(0, 10)
+      const id = json.id ?? nanoid()
+      const createdAt = Date.now()
+      const path = `/xat/${id}`
       const payload = {
         id,
         title,
@@ -52,18 +52,18 @@ export async function POST(req: Request) {
         messages: [
           {
             content: completion,
-            role: "assistant",
+            role: 'assistant'
           },
-          ...messages,
-        ],
-      };
-      await kv.hmset(`chat:${id}`, payload);
+          ...messages
+        ]
+      }
+      await kv.hmset(`chat:${id}`, payload)
       await kv.zadd(`user:chat:${userId}`, {
         score: createdAt,
-        member: `chat:${id}`,
-      });
-    },
-  });
+        member: `chat:${id}`
+      })
+    }
+  })
 
-  return new StreamingTextResponse(stream);
+  return new StreamingTextResponse(stream)
 }
