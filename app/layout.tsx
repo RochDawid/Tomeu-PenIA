cfc95d97bd4786947763c41f934692cb8a0935a5
Dawid Roch <david.roch.2001@gmail.com>
2023-12-01 16:50:30 +0100

    Refactor layout component and remove tailwind-indicator component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,37 @@
-import { Toaster } from "react-hot-toast";
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
+import { Analytics } from '@vercel/analytics/react'
+import { GeistMono } from 'geist/font/mono'
+import { GeistSans } from 'geist/font/sans'
+import { Toaster } from 'react-hot-toast'
 
-import "@/app/globals.css";
-import { cn } from "@/lib/utils";
-import { TailwindIndicator } from "@/components/tailwind-indicator";
-import { Providers } from "@/components/providers";
-import { Header } from "@/components/header";
+import '@/app/globals.css'
+import { Header } from '@/components/header'
+import { Providers } from '@/components/providers'
+import { cn } from '@/lib/utils'
 
 export const metadata = {
   metadataBase: new URL(`https://${process.env.VERCEL_URL}`),
   title: {
-    default: "Tomeu PenIA",
-    template: `%s - Tomeu PenIA`,
+    default: 'Tomeu PenIA',
+    template: `%s - Tomeu PenIA`
   },
   description:
-    "Xerra amb en Tomeu PenIA, una experiència única de conversa virtual.",
+    'Xerra amb en Tomeu PenIA, una experiència única de conversa virtual.',
   icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
-};
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png'
+  }
+}
 
 export const viewport = {
   themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-};
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' }
+  ]
+}
 
 interface RootLayoutProps {
-  children: React.ReactNode;
+  children: React.ReactNode
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
@@ -39,9 +39,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
     <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
-          "font-sans antialiased",
+          'font-sans antialiased',
           GeistSans.variable,
-          GeistMono.variable,
+          GeistMono.variable
         )}
       >
         <Toaster />
@@ -50,9 +50,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
             <Header />
             <main className="flex flex-col flex-1 bg-muted/50">{children}</main>
           </div>
-          <TailwindIndicator />
+          <Analytics />
         </Providers>
       </body>
     </html>
-  );
+  )
 }
