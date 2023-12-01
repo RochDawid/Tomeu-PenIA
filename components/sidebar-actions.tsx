3015b3d7a9dc5e2aee8a7e2a216c1dd2bcc65b5e
Dawid Roch <david.roch.2001@gmail.com>
2023-12-01 16:00:43 +0100

    Fix import statements in SidebarActions component

--- components/sidebar-actions.tsx
+++ components/sidebar-actions.tsx
@@ -1,10 +1,8 @@
-"use client";
+'use client'
 
-import { useRouter } from "next/navigation";
-import { toast } from "react-hot-toast";
+import { useRouter } from 'next/navigation'
+import { toast } from 'react-hot-toast'
 
-import { type Chat, ServerActionResult } from "@/lib/types";
-import { cn, formatDate } from "@/lib/utils";
 import {
   AlertDialog,
   AlertDialogAction,
@@ -13,71 +11,73 @@ import {
   AlertDialogDescription,
   AlertDialogFooter,
   AlertDialogHeader,
-  AlertDialogTitle,
-} from "@/components/ui/alert-dialog";
-import { Button } from "@/components/ui/button";
+  AlertDialogTitle
+} from '@/components/ui/alert-dialog'
+import { badgeVariants } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
 import {
   Dialog,
   DialogContent,
   DialogDescription,
   DialogFooter,
   DialogHeader,
-  DialogTitle,
-} from "@/components/ui/dialog";
+  DialogTitle
+} from '@/components/ui/dialog'
 import {
   IconShare,
   IconSpinner,
   IconTrash,
-  IconUsers,
-} from "@/components/ui/icons";
-import Link from "next/link";
-import { badgeVariants } from "@/components/ui/badge";
+  IconUsers
+} from '@/components/ui/icons'
 import {
   Tooltip,
   TooltipContent,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
-import { useCallback, useState, useTransition } from "react";
+  TooltipTrigger
+} from '@/components/ui/tooltip'
+import { ServerActionResult, type Chat } from '@/lib/types'
+import { cn, formatDate } from '@/lib/utils'
+import Link from 'next/link'
+import { useCallback, useState, useTransition } from 'react'
 
 interface SidebarActionsProps {
-  chat: Chat;
-  removeChat: (args: { id: string; path: string }) => ServerActionResult<void>;
-  shareChat: (chat: Chat) => ServerActionResult<Chat>;
+  chat: Chat
+  removeChat: (args: { id: string; path: string }) => ServerActionResult<void>
+  shareChat: (chat: Chat) => ServerActionResult<Chat>
 }
 
 export function SidebarActions({
   chat,
   removeChat,
-  shareChat,
+  shareChat
 }: SidebarActionsProps) {
-  const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
-  const [shareDialogOpen, setShareDialogOpen] = useState(false);
-  const [isRemovePending, startRemoveTransition] = useTransition();
-  const [isSharePending, startShareTransition] = useTransition();
-  const router = useRouter();
+  const [deleteDialogOpen, setDeleteDialogOpen] = useState(false)
+  const [shareDialogOpen, setShareDialogOpen] = useState(false)
+  const [isRemovePending, startRemoveTransition] = useTransition()
+  const [isSharePending, startShareTransition] = useTransition()
+  const router = useRouter()
 
   const copyShareLink = useCallback(async (chat: Chat) => {
     if (!chat.sharePath) {
-      return toast.error("No s'ha pogut copiar l'enllaç 😓");
+      return toast.error("No s'ha pogut copiar l'enllaç 😓")
     }
 
-    const url = new URL(window.location.href);
-    url.pathname = chat.sharePath;
-    navigator.clipboard.writeText(url.toString());
-    setShareDialogOpen(false);
-    toast.success("Enllaç copiat amb èxit 🥳", {
+    const url = new URL(window.location.href)
+    url.pathname = chat.sharePath
+    navigator.clipboard.writeText(url.toString())
+    setShareDialogOpen(false)
+    toast.success('Enllaç copiat amb èxit 🥳', {
       style: {
-        borderRadius: "10px",
-        background: "#333",
-        color: "#fff",
-        fontSize: "14px",
+        borderRadius: '10px',
+        background: '#333',
+        color: '#fff',
+        fontSize: '14px'
       },
       iconTheme: {
-        primary: "white",
-        secondary: "black",
-      },
-    });
-  }, []);
+        primary: 'white',
+        secondary: 'black'
+      }
+    })
+  }, [])
 
   return (
     <>
@@ -113,15 +113,15 @@ export function SidebarActions({
       <Dialog open={shareDialogOpen} onOpenChange={setShareDialogOpen}>
         <DialogContent>
           <DialogHeader>
-            <DialogTitle>Compartir enllaç al xat</DialogTitle>
+            <DialogTitle>Compartir el xat</DialogTitle>
             <DialogDescription>
-              Qualsevol persona amb l&amp;enllaç podrà veure el xat.
+              Qualsevol persona amb l&apos;enllaç podrà veure el xat.
             </DialogDescription>
           </DialogHeader>
           <div className="space-y-1 rounded-md border p-4 text-sm">
             <div className="font-medium">{chat.title}</div>
             <div className="text-muted-foreground">
-              {formatDate(Number(chat.createdAt))} · {chat.messages.length}{" "}
+              {formatDate(Number(chat.createdAt))} · {chat.messages.length}{' '}
               missatges
             </div>
           </div>
@@ -130,8 +130,8 @@ export function SidebarActions({
               <Link
                 href={chat.sharePath}
                 className={cn(
-                  badgeVariants({ variant: "secondary" }),
-                  "mr-auto",
+                  badgeVariants({ variant: 'secondary' }),
+                  'mr-auto'
                 )}
                 target="_blank"
               >
@@ -144,20 +144,20 @@ export function SidebarActions({
               onClick={() => {
                 startShareTransition(async () => {
                   if (chat.sharePath) {
-                    await new Promise((resolve) => setTimeout(resolve, 500));
-                    copyShareLink(chat);
-                    return;
+                    await new Promise(resolve => setTimeout(resolve, 500))
+                    copyShareLink(chat)
+                    return
                   }
 
-                  const result = await shareChat(chat);
+                  const result = await shareChat(chat)
 
-                  if (result && "error" in result) {
-                    toast.error(result.error);
-                    return;
+                  if (result && 'error' in result) {
+                    toast.error(result.error)
+                    return
                   }
 
-                  copyShareLink(result);
-                });
+                  copyShareLink(result)
+                })
               }}
             >
               {isSharePending ? (
@@ -166,7 +166,7 @@ export function SidebarActions({
                   Copiant...
                 </>
               ) : (
-                <>Copiar enllaç</>
+                <>Copiar l&apos;enllaç</>
               )}
             </Button>
           </DialogFooter>
@@ -187,24 +187,24 @@ export function SidebarActions({
             </AlertDialogCancel>
             <AlertDialogAction
               disabled={isRemovePending}
-              onClick={(event) => {
-                event.preventDefault();
+              onClick={event => {
+                event.preventDefault()
                 startRemoveTransition(async () => {
                   const result = await removeChat({
                     id: chat.id,
-                    path: chat.path,
-                  });
+                    path: chat.path
+                  })
 
-                  if (result && "error" in result) {
-                    toast.error(result.error);
-                    return;
+                  if (result && 'error' in result) {
+                    toast.error(result.error)
+                    return
                   }
 
-                  setDeleteDialogOpen(false);
-                  router.refresh();
-                  router.push("/");
-                  toast.success("Xat eliminat 🗑️");
-                });
+                  setDeleteDialogOpen(false)
+                  router.refresh()
+                  router.push('/')
+                  toast.success('Xat eliminat 🗑️')
+                })
               }}
             >
               {isRemovePending && <IconSpinner className="mr-2 animate-spin" />}
@@ -214,5 +214,5 @@ export function SidebarActions({
         </AlertDialogContent>
       </AlertDialog>
     </>
-  );
+  )
 }
